b941b3fcb8dbce1d7c6304916ad6e2692b8b7c71
PukhalskyiDmytro <pukhalskyi@knu.ua>
2025-03-04 12:38:13 +0200

    Deleted redundant files

--- hello_world.py
+++
@@ -1,5 +0,0 @@
-def print_hello_world():
-    print("Hello world")
-
-for i in range(10):
-    print_hello_world()
--- main.py
+++
@@ -1,5 +0,0 @@
-import utils
-from utils import factorial, is_power_of_five
-
-print(factorial(5))
-print(is_power_of_five(25))
